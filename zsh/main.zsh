4c374c665a3a37f8cb309d33a70e8350a0b061c4
yoshikazusawa <yoshikazu.sawa@gmail.com>
2014-03-17 14:13:09 +0900

    enable more oh-my-zsh plugin

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -16,8 +16,7 @@ ZSH_THEME="mh"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git github node nvm npm perl)
+plugins=(git github node nvm npm perl bower coffee cp tmux )
 
 source $ZSH/oh-my-zsh.sh
 
